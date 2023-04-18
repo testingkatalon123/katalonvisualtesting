0a128dd49b30839bcbe366dc5f4907379c8c5669
Mindy Kim <mkim@solverglobal.com>
2023-04-17 20:49:02 -0700

    WHERE

--- Test Suites/new2.ts
+++ Test Suites/new2.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description>eddssdad</description>
-   <name>new</name>
+   <name>new2</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
