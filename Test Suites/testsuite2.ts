ba5fb1169a9351e3fa71937b114d69199ff406f0
Mindy Kim <mkim@solverglobal.com>
2023-04-17 20:28:34 -0700

    sadfsdfdsfsdf

--- Test Suites/testsuite2.ts
+++ Test Suites/testsuite2.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>f98648dd-f2b6-41e7-9cbc-ea84144cc851</testSuiteGuid>
+   <testCaseLink>
+      <guid>525deac3-1e50-4ade-adc4-aea9be81def2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/screenshotpls</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
