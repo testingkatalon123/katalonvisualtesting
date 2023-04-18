e2458765325d473e64685f253984542f2c89a52e
Mindy Kim <mkim@solverglobal.com>
2023-04-17 20:40:44 -0700

    ddddd

---
+++ Test Suites/new.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>eddssdad</description>
+   <name>new</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>129fc777-72b9-42c4-8104-300b8e15a773</testSuiteGuid>
+</TestSuiteEntity>
