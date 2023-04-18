81507e562b9bb217755f5e91fa111122d1e1d0f4
Mindy Kim <mkim@solverglobal.com>
2023-04-17 20:36:54 -0700

    fedsuhi

--- Test Suites/testsuite.ts
+++ Test Suites/testsuite.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
    <description></description>
-   <name>testsuite2</name>
+   <name>testsuite</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
