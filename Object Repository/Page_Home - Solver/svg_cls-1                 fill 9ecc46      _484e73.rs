2b0ead5deaa782287410f87bbcc9c32a7b82c77e
Mindy Kim <mkim@solverglobal.com>
2023-04-17 19:05:19 -0700

    hmmmm

---
+++ Object Repository/Page_Home - Solver/svg_cls-1                 fill 9ecc46      _484e73.rs
@@ -0,0 +1,190 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>svg_cls-1                 fill 9ecc46      _484e73</name>
+   <tag></tag>
+   <elementGuidId>baa0eecb-5006-4519-93a4-d97cf1432e29</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>#logo-landscape</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Home'])[1]/preceding::*[name()='svg'][1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>svg</value>
+      <webElementGuid>0544e760-5dda-46e2-a4f1-2552c83eba61</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>icon</name>
+      <type>Main</type>
+      <value>logo-landscape</value>
+      <webElementGuid>c3be6ea1-e65e-47c2-ac7b-31d432363df9</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>icon icon--dark</value>
+      <webElementGuid>e9e5099f-3421-4352-98f0-3b5cd4eea5e3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>logo-landscape</value>
+      <webElementGuid>13947444-8eb4-400a-b82f-641399058bf4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>data-name</name>
+      <type>Main</type>
+      <value>Layer 1</value>
+      <webElementGuid>cf624d56-8c50-4d2c-961d-29a9c871fe51</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xmlns</name>
+      <type>Main</type>
+      <value>http://www.w3.org/2000/svg</value>
+      <webElementGuid>22246909-768c-45e5-9751-99278828957e</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>viewBox</name>
+      <type>Main</type>
+      <value>0 0 680 177.02</value>
+      <webElementGuid>41ebde1f-fd09-4b7d-8402-5e091643a758</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+        
+            .cls-1 {
+                fill: #9ecc46;
+            }
+
+            .cls-2 {
+                fill: #112A4E;
+            }
+
+            .cls-3 {
+                fill: #1c6bb5;
+            }
+        
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+</value>
+      <webElementGuid>0c9c7f6c-8bb0-4b02-9f9a-fbdce525db06</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;logo-landscape&quot;)</value>
+      <webElementGuid>32a8a8c8-2749-4b7f-8d98-e42a58e54a49</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Home'])[1]/preceding::*[name()='svg'][1]</value>
+      <webElementGuid>95d77642-bf8b-4c01-b95a-85211ac7b1f8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Notifications'])[1]/preceding::*[name()='svg'][1]</value>
+      <webElementGuid>7361f714-ae78-4be4-a32d-c079833b5337</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//svg[@id = 'logo-landscape' and (text() = '
+    
+        
+            .cls-1 {
+                fill: #9ecc46;
+            }
+
+            .cls-2 {
+                fill: #112A4E;
+            }
+
+            .cls-3 {
+                fill: #1c6bb5;
+            }
+        
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+' or . = '
+    
+        
+            .cls-1 {
+                fill: #9ecc46;
+            }
+
+            .cls-2 {
+                fill: #112A4E;
+            }
+
+            .cls-3 {
+                fill: #1c6bb5;
+            }
+        
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+')]</value>
+      <webElementGuid>b4eb7792-a708-4a93-aec5-97fde577a359</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/testsuite2.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>testsuite2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>f98648dd-f2b6-41e7-9cbc-ea84144cc851</testSuiteGuid>
+</TestSuiteEntity>
